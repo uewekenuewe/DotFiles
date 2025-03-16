590c0f3c77ae48326ad7746ee9301c9ec2cbfcdd
= <derFeind87@gmail.com>
2025-03-16 15:23:43 +0100

    change setup windows // linux // add first stuff for windows

---
+++ windows/install.bat
@@ -0,0 +1,1 @@
+echo * install bat starting
---
+++ windows/powershell/Microsoft.Powershell_profile.ps1
@@ -0,0 +1,16 @@
+cd C:\projects
+
+Set-Alias ts tree-sitter
+function home{
+     cd C:\Users\derFe\
+    }
+function vimrc{
+     cd C:\Users\derFe\AppData\Local\nvim\
+    }
+
+function projects{
+    cd C:\projects
+}
+
+Import-Module 'C:\projects\posh-git\src\posh-git.psd1'
+
